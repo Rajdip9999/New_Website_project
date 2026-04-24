235d302587a17250332ec665ad3cbb85930821df
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2026-04-24 08:10:12 +0530

    Remove logic related to state evaluation

--- dd/logic.js
+++ dd/logic.js
@@ -1,14 +1,1 @@
-const state = 'Rajasthan'
 
-
-if(state === "Rajasthan"){
-    console.log('This is lerger city')
-}
-
-else if(state != "Rajasthan"){
-    console.log('Not big City')
-}
-
-else{
-    console.log('R u fool?')
-}
